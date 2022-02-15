d36ab587dc308cfa72e1d97d40fb255040a46b9e
simple <simpleyzh@gmail.com>
2022-02-15 18:58:26 +0800

    freemarker

--- framework/ouath2/src/main/java/com/test/justauth/config/ImageConfig.java
+++ framework/ouath2/src/main/java/com/test/justauth/config/ImageConfig.java
@@ -1,13 +1,12 @@
 package com.test.justauth.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author simple
  */
-@Configuration
+//@Configuration
 public class ImageConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
